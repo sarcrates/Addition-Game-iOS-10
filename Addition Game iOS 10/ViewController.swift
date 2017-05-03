4e371dace18b92dad5e972caaf5da6300b30173f
Hana Sakamoto <sarcrates@sarcratess-MacBook-Pro.local>
2017-05-03 10:11:58 -0700

    Addition Game For iOS 10
    Updated to work with iOS 10 and works smoothly.

---
+++ Addition Game iOS 10/ViewController.swift
@@ -0,0 +1,216 @@
+//
+//  ViewController.swift
+//  Addition Game iOS 10
+//
+//  Created by sarcrates on 4/29/17.
+//  Copyright © 2017 sarcrates. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+	@IBOutlet weak var AddtionGameLabel: UILabel!
+	@IBOutlet weak var QuestionLabel: UILabel!
+	@IBOutlet weak var CorrectTotalLabel: UILabel!
+	@IBOutlet weak var CorrectIncorrectLabel: UILabel!
+	
+	@IBOutlet weak var Answer0: UIButton!
+	@IBOutlet weak var Answer1: UIButton!
+	@IBOutlet weak var Answer2: UIButton!
+	@IBOutlet weak var Answer3: UIButton!
+	
+	@IBOutlet weak var achievementlabel: UILabel!
+	
+	
+	var firstNumber : Int = 0
+	var secondNumber : Int = 0
+	var answer : Int = 0
+	
+	var correctTotal : Int = 0
+	var correctIncorrect = ""
+	
+	var RandomButton : Int = 0
+	
+	var incorrectAnswer1 : Int = 0
+	var incorrectAnswer2 : Int = 0
+	var incorrectAnswer3 : Int = 7
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		
+		randomNumbers()
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	func randomNumbers(){
+		firstNumber = Int(arc4random_uniform(9))
+		secondNumber = Int(arc4random_uniform(9))
+		answer = firstNumber + secondNumber
+		
+		incorrectAnswer1 = Int(arc4random_uniform(5))
+		incorrectAnswer2 = Int(arc4random_uniform(2))
+		RandomButton = Int(arc4random_uniform(4))
+		
+		printMathQuestion()
+		ShowTextOnButton()
+		showCorrectIncorrect()
+	}
+	func RandomCheck(){
+		if answer == incorrectAnswer1 || answer == incorrectAnswer2 || incorrectAnswer1 == incorrectAnswer2{
+			incorrectAnswer1 = Int(arc4random_uniform(3))
+			incorrectAnswer2 = 0
+			
+			
+			if answer == incorrectAnswer1 || answer == incorrectAnswer2 || incorrectAnswer1 == incorrectAnswer2{
+				incorrectAnswer1 = 20
+				incorrectAnswer2 = 21
+			}
+			
+		}
+		
+		
+	}
+	
+	func printMathQuestion(){
+		QuestionLabel.text = "\(firstNumber) + \(secondNumber) = ?"
+	}
+
+	func ShowTextOnButton(){
+		if RandomButton == 0{
+			
+			Answer0.setTitle("\(answer)", for: UIControlState())
+			Answer1.setTitle("\(incorrectAnswer1)", for: UIControlState())
+			Answer2.setTitle("\(incorrectAnswer2)", for: UIControlState())
+			Answer3.setTitle("\(incorrectAnswer3)", for: UIControlState())
+			
+		}
+		if RandomButton == 1{
+			
+			Answer1.setTitle("\(answer)", for: UIControlState())
+			Answer0.setTitle("\(incorrectAnswer1)", for: UIControlState())
+			Answer2.setTitle("\(incorrectAnswer2)", for: UIControlState())
+			Answer3.setTitle("\(incorrectAnswer3)", for: UIControlState())
+			
+		}
+		if RandomButton == 2{
+			
+			Answer2.setTitle("\(answer)", for: UIControlState())
+			Answer1.setTitle("\(incorrectAnswer3)", for: UIControlState())
+			Answer0.setTitle("\(incorrectAnswer2)", for: UIControlState())
+			Answer3.setTitle("\(incorrectAnswer1)", for: UIControlState())
+			
+		}
+		if RandomButton == 3{
+			
+			Answer3.setTitle("\(answer)", for: UIControlState())
+			Answer1.setTitle("\(incorrectAnswer1)", for: UIControlState())
+			Answer2.setTitle("\(incorrectAnswer2)", for: UIControlState())
+			Answer0.setTitle("\(incorrectAnswer3)", for: UIControlState())
+			
+		}
+		
+	}
+	
+	@IBAction func Answer0Action(_ sender: UIButton) {
+		if RandomButton == 0{
+			button0correct()
+		}
+		else if RandomButton != 0{
+			gotincorrectAnswer()
+		}
+		
+		randomNumbers()
+		
+	}
+	@IBAction func Answer1Action(_ sender: UIButton) {
+		if RandomButton == 1{
+			button1correct()
+		}
+		else if RandomButton != 1{
+			gotincorrectAnswer()
+		}
+		
+		randomNumbers()
+		
+	}
+	@IBAction func Answer2Action(_ sender: UIButton) {
+		if RandomButton == 2{
+			button2correct()
+		}
+		else if RandomButton != 2{
+			gotincorrectAnswer()
+		}
+		
+		randomNumbers()
+		
+	}
+
+	@IBAction func Answer3Action(_ sender: UIButton) {
+		if RandomButton == 3{
+			button3correct()
+		}
+		else if RandomButton != 3{
+			gotincorrectAnswer()
+		}
+		
+		randomNumbers()
+		
+	}
+	func button0correct(){
+		checkingForCorrectAnswer()
+	}
+	func button1correct(){
+		checkingForCorrectAnswer()
+	}
+	
+	func button2correct(){
+		checkingForCorrectAnswer()
+	}
+	func button3correct(){
+		checkingForCorrectAnswer()
+	}
+	func gotincorrectAnswer(){
+		correctIncorrect = "Incorrect"
+	}
+	
+	func checkingForCorrectAnswer(){
+		correctTotal = correctTotal + 1
+		correctIncorrect = "Correct"
+//		playSound()
+	}
+	
+	func showCorrectIncorrect(){
+		CorrectTotalLabel.text = "Total Correct: \(correctTotal)"
+		CorrectIncorrectLabel.text = "\(correctIncorrect)"
+	}
+	
+	@IBAction func ResetAction(_ sender: UIButton) {
+		correctTotal = 0
+		CorrectTotalLabel.text = "Total Correct: \(correctTotal)"
+	}
+	
+	
+//	var player: AVAudioPlayer?
+//	func playSound(){
+//		guard let url = Bundle.main.url(forResource: "Ding", withExtension: "m4a") else {
+//			print("not playing")
+//			return
+//		}
+//		do {
+
+//			try! AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+//			try! AVAudioSession.sharedInstance().setActive(true)
+//			player = try! AVAudioPlayer.init(contentsOf: url, fileTypeHint: AVFileTypeMPEG4)
+//			player!.play()
+//		} catch let error as NSError{
+//			print ("error: \(error.localizedDescription)")
+//		}
+//	}
+}
+
---
+++ Addition Game iOS 10Tests/Addition_Game_iOS_10Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Addition_Game_iOS_10Tests.swift
+//  Addition Game iOS 10Tests
+//
+//  Created by sarcrates on 4/29/17.
+//  Copyright © 2017 sarcrates. All rights reserved.
+//
+
+import XCTest
+@testable import Addition_Game_iOS_10
+
+class Addition_Game_iOS_10Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
